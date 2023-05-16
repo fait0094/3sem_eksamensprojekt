4c86defdd49086ce8cc9e34ce8011de43560091c
Faith Mansilungan <faith0094@stud.kea.dk>
2023-05-16 17:50:09 +0200

    Color styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary_blue: "#1F9ECD",
+        primary_black: "#323332",
+        primary_white: "#FCFCFC",
+        secondary_beige: "#f1f1e8",
+        secondary_eggwhite: "#fcfbfa",
+        accent_green: "#2ca900",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        barlow: ["Barlow", "sans-serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
